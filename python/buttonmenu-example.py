44db7eaf59265c15a273b6a39d5a0f6b2046b418
yu205619 <205619@learning.gsis.edu.hk>
2020-01-17 16:41:25 +0800

    fixed display issues

--- python/buttonmenu-example.py
+++ python/buttonmenu-example.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env pybricks-micropython
 
-from buttonmenu import ButtonMenu
-
+# sample functions start
 import time
-
 from pybricks.parameters import Button
 from pybricks.parameters import Color
-
 from pybricks import ev3brick
-
 def wait_time(zeit):
 	time.sleep(zeit)
-
 def cool_lights():
 	ev3brick.light(Color.RED)
 	time.sleep(1)
 	ev3brick.light(Color.BLACK)
-
-def move_tank(power,angle):
-	print("moving the tank at power {} and angle {}".format(power,angle))
+def text(first,second):
+	print("{}:{}".format(first,second))
 	time.sleep(1)
-
 def scream():
 	ev3brick.sound.beep()
 	time.sleep(1)
+# sample functions end
 
 
-# check this out
+
+## USAGE
+# import the module
+from buttonmenu import ButtonMenu
+# create ButtonMenu object
 runmenu = ButtonMenu()
-runmenu.set(Button.LEFT,wait_time,(10,),"wait")
+# set button actions:
+# <ButtonMenu object>.set( <Button>, <function>, <function arguments as tuple>, <name> )
+runmenu.set(Button.LEFT,wait_time,(10,),"wait") # for single arguments, add trailing comma
 runmenu.set(Button.RIGHT,cool_lights,(),"lights")
-runmenu.set(Button.UP,move_tank,(100,12))
-runmenu.set(Button.DOWN,scream,name="scream")
+runmenu.set(Button.UP,text,(100,12)) # name defaults to "untitled"
+runmenu.set(Button.DOWN,scream,name="scream") # arguments defaults to empty
 runmenu.run()
--- python/buttonmenu.py
+++ python/buttonmenu.py
@@ -14,22 +14,24 @@ class ButtonMenu:
 		self.programs[button][2] = args
 	def run(self):
 		self.display()
+		# wait for button
 		while True:
-			# wait for button
-			while True:
-				buttons = ev3brick.buttons()
-				if any(buttons) and buttons[0] in self.programs:
-					# a button is pressed
-					if self.programs[buttons[0]][1]: # ensure program is not None
-						self.programs[buttons[0]][1](*self.programs[buttons[0]][2]) # program(args)
+			buttons = ev3brick.buttons()
+			if any(buttons) and buttons[0] in self.programs:
+				# a button is pressed
+				if self.programs[buttons[0]][1]: # ensure program is not None
+					self.programs[buttons[0]][1](*self.programs[buttons[0]][2]) # program(args)
+					# redraw screen after function is run
+					self.display()
 	def display(self):
+		# 11 chars per space
 		ev3brick.display.clear()
 		for btn,loc in { # buttons paired with location on screen
-			Button.LEFT: (0,60),
-			Button.RIGHT: (89,60),
-			Button.UP: (20,0),
-			Button.DOWN: (20,167)
+			Button.LEFT: (0,70),
+			Button.RIGHT: (90,70),
+			Button.UP: (45,10),
+			Button.DOWN: (45,125)
 		}.items():
 			ev3brick.display.text(
-				self.programs[btn][0], # name of program
+				"{:^11.11}".format(self.programs[btn][0]), # name of program
 				loc)
